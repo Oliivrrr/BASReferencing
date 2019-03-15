f885d26354e7e6cefb3b24cd4c41ebfe7be9a3f4
Unlucky <flagellaiscool@gmail.com>
2019-03-15 15:05:22 +1100

    Added Website Menu

--- BASReferencing/Main.Designer.cs
+++ BASReferencing/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BASReferencing
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Main";
+            this.Text = "BAS Referencer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- BASReferencing/Website.cs
+++ BASReferencing/Website.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BASReferencing
 {
-    public partial class Form1 : Form
+    public partial class Website : Form
     {
-        public Form1()
+        public Website()
         {
             InitializeComponent();
         }
